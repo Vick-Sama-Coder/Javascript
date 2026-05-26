3b8227fa11db092a7b5e745a75e2df1196ffa73d
Vick-Sama-Coder <vickeldaelisa150@gmail.com>
2026-05-26 14:38:22 +0200

    Functions and Objects

---
+++ Vanilla /object.js
@@ -0,0 +1,8 @@
+        let obj = {
+            nome:"Vickel",
+            idade:18,
+            sexo:"Masculino",
+            profissao:"Dev"
+        };
+
+        console.log(localStorage.setItem(JSON.stringify(obj)))
