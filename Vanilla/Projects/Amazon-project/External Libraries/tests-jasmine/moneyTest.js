1dafcd4c92450b8ffa709765f0fc32bdd04d261c
Vick-Sama-Coder <vickeldaelisa150@gmail.com>
2026-06-19 15:02:30 +0200

    feat: test the currency formating with Jasmine

---
+++ Vanilla/Projects/Amazon-project/External Libraries/tests-jasmine/moneyTest.js
@@ -0,0 +1,16 @@
+import { fixCurrency } from "../../Scripts/Utils/money.js"
+
+
+describe('test suite :  fix Currency', () =>{
+    it('converts cents into Dollars', () =>{
+        expect(fixCurrency(2089)).toEqual('20.89')
+    });
+
+    it('works With 0', () => {
+        expect(fixCurrency(0)).toEqual('0.00')
+    });
+
+    it('rounds up to the nearest cent', () => {
+        expect(fixCurrency(3000.9)).toEqual('30.01')
+    })
+})
